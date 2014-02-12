5d30f6c5935fd6b1f2c2ed14279ec0a8609b297b
Ted <tedkim@stanford.edu>
2014-02-12 23:40:46 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -12,6 +12,8 @@ function initializePage() {
 	$('.project a').click(addProjectDetails);
 
 	$('#colorBtn').click(randomizeColors);
+
+	$('#mapBtn').click(getPhotos);
 }
 
 /*
@@ -27,6 +29,29 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	// Make ajax call
+	var url = "/project/" + idNumber;
+	console.log("URL: " + url);
+	$.get(url, projectDetailsCallback)
+}
+
+// Callback from project details ajax call
+function projectDetailsCallback(result) {
+	console.log(result);
+
+	// detailsImage class
+	// image, small header with the date, summary
+
+	var detailsHTML = '<img src="' + result['image'] + '"class="detailsImage">' +
+		//'<p><small>' + result['date'] + '</small></p>' +
+		'<h4>' + result['date'] + '</h4>' +
+		result['summary'];
+
+
+	$("#project"+result['id']+" .details").html(detailsHTML);
+
+	console.log(detailsHTML);
 }
 
 /*
@@ -35,4 +60,43 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	var url = "/palette";
+	$.get(url, colorsCallback)
 }
+
+// Callback from colors ajax call
+function colorsCallback(result) {
+	console.log(result);
+
+	var colors = result.colors.hex;
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+}
+
+/*
+ * Make an AJAX call to get photos from where I'm from
+ */
+function getPhotos(e) {
+	console.log("User clicked on map photos button");
+
+	var url = "http://www.panoramio.com/map/get_panoramas.php?set=public&from=0&to=1&minx=124&miny=35&maxx=128&maxy=39&size=medium";
+	$.get(url, externalCallback, 'jsonp')
+
+}
+
+// Callback for external stretch goal ajax call
+function externalCallback(result) {
+	console.log(result);
+	var detailsHTML = '<h4>Latest Photo From Seoul</h4>' + 
+		'<img src="' + result.photos[0].photo_file_url + '"class="detailsImage">' +
+		//'<p><small>' + result['date'] + '</small></p>' +
+		'<h6>' + result.photos[0].upload_date + '</h4>';
+
+
+	$("#fromPhotos").html(detailsHTML);
+}
+
